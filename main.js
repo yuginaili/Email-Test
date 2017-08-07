5fd4b0993cd2cd81cb8e9ca60747d533030445de
Ricky Lung <dard12@gmail.com>
2017-08-07 18:28:30 -0400

    lets make this harder

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 /*
   TODO:
-  1. Grab all emails using fetchEmailsFromDatabase
+  1. Grab all emails using fetchEmailsFromDatabase. (HINT: make use of nextPage)
   2. Run the fetched emails through getFilteredEmails
   3. Implement getFilteredEmails
 */
 
 function render() {
   // Your Code Here
-
-  renderEmails();
 }
 
 /*
   Emails are of format { author: String, subject: String, body: String }
 
   args:
-    allEmails: [Email] All emails fetched from fetchEmailsFromDatabase
-    searchInputs: [Strings] Inputs to filter allEmails on
+    allEmails: [Email], All emails fetched from fetchEmailsFromDatabase
+    searchInputs: [Strings], Inputs to filter allEmails on
 
   return: [Email]
           All emails that have at least one mapped value
@@ -29,11 +27,26 @@ function getFilteredEmails(allEmails = [], searchInputs = getSearchInputs()) {
 
 //  ------------ Do Not Make Changes Below This Line ------------
 
-function fetchEmailsFromDatabase(callback) {
+/*
+  args:
+    params: Object, { page: Integer }, the current page of emails being fetched from database
+    callback: Function, with signature (error, result) from the database
+*/
+function fetchEmailsFromDatabase(params = { page: 0 }, callback) {
   const emails = [
     {
       author: 'Bobby Bob',
       subject: 'Hey Friend!',
+      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      author: 'Bobby Not-Bob',
+      subject: 'Hey Friend!',
+      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    },
+    {
+      author: 'Bobby Obo',
+      subject: 'Hey Friendo!',
       body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     },
     {
@@ -42,34 +55,32 @@ function fetchEmailsFromDatabase(callback) {
       body: `ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
     },
     {
-      author: 'Some Guy',
-      subject: 'Please buy my product.',
-      body: `My product is the best. For just $1,000 you could buy my product and make me somewhat richer. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
-    },
-
-    {
-      author: 'Bobby Not-Bob',
-      subject: 'Hey Friend!',
-      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
-    },
-    {
       author: 'Jenny Janey',
       subject: 'Let Jenny know if you are planning...',
       body: `ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-    },
-    {
-      author: 'Bobby Obo',
-      subject: 'Hey Friendo!',
-      body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     },
     {
       author: 'Some Guy',
       subject: 'Please DO NOT buy my product.',
       body: `My product is a scam. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
     },
+    {
+      author: 'Some Guy',
+      subject: 'Please buy my product.',
+      body: `My product is the best. For just $1,000 you could buy my product and make me somewhat richer. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud.`,
+    },
   ];
 
-  callback(emails);
+  setTimeout(() => {
+    const page = params.page;
+    const nextPage = Math.min(page + _.random(1, 3), emails.length);
+    const fetchedEmails = _.slice(emails, page, nextPage);
+
+    callback(null, {
+      emails: fetchedEmails,
+      nextPage: page !== emails.length ? page : null,
+    });
+  }, 100);
 }
 
 function renderEmails(emails) {
